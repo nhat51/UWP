3c3f3c6946251c9e5db4399ab9ac6970bf1733e2
nhat51 <65001571+nhat51@users.noreply.github.com>
2021-05-26 17:15:04 +0700

    Lab2

---
+++ lab2/lab2/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using lab2.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace lab2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+       
+       
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            User u1 = new User("Nhật", "nhat1232");
+            User u2 = new User("Hoàng", "hoangton123");
+
+            ListUsers.Items.Add(u1);
+            ListUsers.Items.Add(u2);
+        }
+    }
+}
---
+++ lab2/lab2/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2.Models
+{
+    class User
+    {
+        private string name;
+        private string email;
+        
+        public User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        } 
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+    }
+}
